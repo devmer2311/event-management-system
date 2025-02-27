e53a8e52ff85e425db735a9535c2b0dbe7645dc6
Dev Mer <138443145+devmer2311@users.noreply.github.com>
2025-02-27 21:04:37 +0530

    Update route.ts

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -73,6 +73,8 @@ export async function POST(req: Request) {
       await clerkClient.users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id,
+        },
+        privateMetadata: {
           "admin":true,
         },
       });
