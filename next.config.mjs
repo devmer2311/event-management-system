16f559ffccf76bde43f777649eb5bbb4a659b241
Dev Mer <138443145+devmer2311@users.noreply.github.com>
2025-02-27 13:16:15 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,24 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *", 
+          
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
